65ac750ed6dcf0f398286d228ff95173cf55a10e
NoobMahbub <Mahbubbengali@gmail.com>
2025-03-13 15:09:48 +0600

    added searchbar to courses page

--- src/Pages/Courses/Courses.jsx
+++ src/Pages/Courses/Courses.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Banner from './Banner/Banner'
+import Search from '../../Shared/Search'
 
 
 const Courses = () => {
   return (
-    <div>
+    <div className='bg-[#000000]'>
         <Banner/>
+        <Search/>
     </div>
   )
 }
