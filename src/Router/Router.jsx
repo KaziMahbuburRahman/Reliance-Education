b5d82899a5280bc38c791977db4427bf604e14ce
NoobMahbub <Mahbubbengali@gmail.com>
2025-03-16 02:08:16 +0600

    added routes

---
+++ src/Router/Router.jsx
@@ -0,0 +1,27 @@
+import { Route, Routes } from "react-router-dom";
+import Navbar from "../components/shared/Navbar";
+
+const App = () => {
+  return (
+    <div>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<div>Hello Home</div>} />
+        <Route path="/courses" element={<div>Hello Courses</div>} />
+        <Route path="/about-us" element={<div>Hello About Us</div>} />
+        <Route path="/universities" element={<div>Hello Universities</div>} />
+        <Route path="/scholarships" element={<div>Hello Scholarships</div>} />
+        <Route path="/associates" element={<div>Hello Associates</div>} />
+        <Route
+          path="/student-service"
+          element={<div>Hello Student Service</div>}
+        />
+        <Route path="/contact" element={<div>Hello Contact</div>} />
+        <Route path="*" element={<div>Hello Not Found</div>} />{" "}
+        {/* Optional for 404 */}
+      </Routes>
+    </div>
+  );
+};
+
+export default App;
