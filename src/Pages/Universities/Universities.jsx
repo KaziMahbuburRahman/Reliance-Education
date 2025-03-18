a2c4d8bbd92a917a9f5ebd7d211ef3ca738c1fbe
NoobMahbub <Mahbubbengali@gmail.com>
2025-03-19 01:58:54 +0600

    created universities page

---
+++ src/Pages/Universities/Universities.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import UniversitiesList from "./List/UniversitiesList";
+
+const Universities = () => {
+  return (
+    <div>
+      <UniversitiesList />
+    </div>
+  );
+};
+
+export default Universities;
