588ba0bc07be03384fabecb886106764e2bb0dc7
NoobMahbub <Mahbubbengali@gmail.com>
2025-03-18 17:26:49 +0600

    made coursesbanner mobile responsive

--- src/Pages/Courses/Banner/CoursesBanner.jsx
+++ src/Pages/Courses/Banner/CoursesBanner.jsx
@@ -3,7 +3,7 @@ const CoursesBanner = () => {
     <div>
       <div>
         <div
-          className="md:h-[400px] h-[400px] bg-cover bg-center relative"
+          className="h-[12.5rem] md:h-[400px] bg-cover bg-center relative"
           style={{
             backgroundImage:
               "url('https://www.totalstudentcare.com/uploades/banner/original/find-course-new-banner0.jpg')",
@@ -13,11 +13,11 @@ const CoursesBanner = () => {
           <div className="absolute top-0 left-0 w-full h-full bg-black/50 z-10"></div>
 
           <div className="relative z-20 flex justify-center flex-col sm:flex-row px-3 sm:px-30 gap-6 sm:gap-44 sm:items-start">
-            <div className="md:mt-10 mt-0">
-              <h1 className="text-center font-[700] justify-center text-white text-[2.5rem]  md:text-[4rem] md:p-2 p-0 md:mt-24 mt-10">
+            <div className="md:mt-10 mt-0 flex justify-center items-center flex-col">
+              <h1 className="text-center font-[700] text-white text-3xl  md:text-[4rem] md:p-2 p-0 md:mt-24 mt-10">
                 Study <span className="text-[#e41f3f]">UK courses</span>
               </h1>
-              <p className="text-white text-[1.0625rem] text-center sm:text-xl md:mt-12 mt-10">
+              <p className="text-white text-sm text-center sm:text-xl md:mt-12 mt-10">
                 Foundation, undergraduate, postgraduate taught & research and
                 professional courses to study in the UK
               </p>
