8aeff02dae8db2f85951b9ba75c65bdc09f9af54
NoobMahbub <Mahbubbengali@gmail.com>
2025-03-19 02:47:47 +0600

    updated courses

--- src/Pages/Courses/Courses.jsx
+++ src/Pages/Courses/Courses.jsx
@@ -1,10 +1,10 @@
-import React from "react";
-import Search from "../../Shared/Search";
+"use client"
 
-import { useState } from "react";
-import CoursesBanner from "./Banner/CoursesBanner";
-import CourseFilterSidebar from "./Sidebar/CourseFilterSidebar";
-import CoursesTable from "./Table/CoursesTable";
+import { useState } from "react"
+import CoursesBanner from "./Banner/CoursesBanner"
+import CourseFilterSidebar from "./Sidebar/CourseFilterSidebar"
+import CoursesTable from "./Table/CoursesTable"
+import Search from "../../Shared/Search"
 
 const Courses = () => {
   const [filters, setFilters] = useState({
@@ -14,22 +14,24 @@ const Courses = () => {
     alphabets: [],
     months: [],
     durations: [],
-  });
+  })
 
   // Function to update filters when changed in the sidebar
   const handleFilterChange = (newFilters) => {
-    setFilters(newFilters);
-  };
+    setFilters(newFilters)
+  }
+
   return (
     <div className="bg-[#000000]">
       <CoursesBanner />
       <Search />
-      <div className="flex">
+      <div className="flex flex-col md:flex-row">
         <CourseFilterSidebar onFilterChange={handleFilterChange} />
         <CoursesTable filters={filters} />
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default Courses;
+export default Courses
+
