1c566d3456d5a97d2e31d700b3912dd7ffd5c158
NoobMahbub <Mahbubbengali@gmail.com>
2025-03-18 20:57:11 +0600

    added CourseFilterSidebar and shared props between siblings

--- src/Pages/Courses/Courses.jsx
+++ src/Pages/Courses/Courses.jsx
@@ -1,16 +1,33 @@
 import React from "react";
 import Search from "../../Shared/Search";
 
-
+import { useState } from "react";
 import CoursesBanner from "./Banner/CoursesBanner";
+import CourseFilterSidebar from "./Sidebar/CourseFilterSidebar";
 import CoursesTable from "./Table/CoursesTable";
 
 const Courses = () => {
+  const [filters, setFilters] = useState({
+    category: null,
+    subcategory: null,
+    city: "",
+    alphabets: [],
+    months: [],
+    durations: [],
+  });
+
+  // Function to update filters when changed in the sidebar
+  const handleFilterChange = (newFilters) => {
+    setFilters(newFilters);
+  };
   return (
     <div className="bg-[#000000]">
       <CoursesBanner />
       <Search />
-      <CoursesTable />
+      <div className="flex">
+        <CourseFilterSidebar onFilterChange={handleFilterChange} />
+        <CoursesTable filters={filters} />
+      </div>
     </div>
   );
 };
