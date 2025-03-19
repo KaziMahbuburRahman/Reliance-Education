6139b179138d7c1f32cc5d995dc0cafdd7b004c2
NoobMahbub <Mahbubbengali@gmail.com>
2025-03-20 01:19:29 +0600

    updated coursesBanner text font size for desktop devices

--- src/Pages/Courses/Banner/CoursesBanner.jsx
+++ src/Pages/Courses/Banner/CoursesBanner.jsx
@@ -14,18 +14,16 @@ const CoursesBanner = () => {
 
           <div className="relative z-20 flex justify-center flex-col sm:flex-row px-3 sm:px-30 gap-6 sm:gap-44 sm:items-start">
             <div className="md:mt-10 mt-0 flex justify-center items-center flex-col">
-              <h1 className="text-center font-[700] text-white text-3xl  md:text-[4rem] md:p-2 p-0 md:mt-24 mt-10">
+              <h1 className="text-center font-[700] text-white text-3xl md:text-5xl md:p-2 p-0 md:mt-24 mt-10">
                 Study <span className="text-[#e41f3f]">UK courses</span>
               </h1>
-              <p className="text-white text-sm text-center sm:text-xl md:mt-12 mt-10">
+              <p className="text-white text-sm text-center sm:text-lg md:mt-12 mt-10">
                 Foundation, undergraduate, postgraduate taught & research and
                 professional courses to study in the UK
               </p>
             </div>
           </div>
         </div>
-
-
       </div>
     </div>
   );
