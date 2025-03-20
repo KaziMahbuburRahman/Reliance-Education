ec59371d8f30db2cd246a6944780e8ca7206a8f9
NoobMahbub <Mahbubbengali@gmail.com>
2025-03-20 15:44:55 +0600

    created scholarships page

---
+++ src/Pages/Scholarships/Scholarships.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import ScholarshipList from './List/ScholarshipsList'
+
+const Scholarships = () => {
+  return (
+    <div>
+        <ScholarshipList/>
+    </div>
+  )
+}
+
+export default Scholarships
